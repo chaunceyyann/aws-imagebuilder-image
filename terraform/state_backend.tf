a681753ef0fa6d7cc3a394f49ae559c5af6c428b
Chauncey Yan <chaunceyyann@gmail.com>
2025-05-23 21:07:03 -0400

    minor update on workflow and tf

--- terraform/state_backend.tf
+++ terraform/state_backend.tf
@@ -1,17 +1,9 @@
-resource "aws_s3_bucket" "tfstate" {
+resource "aws_s3_bucket" "terraform_state" {
   bucket = "docker-image-4codebuild-tfstate"
-  acl    = "private"
 
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
   }
 
   tags = {
@@ -19,7 +11,24 @@ resource "aws_s3_bucket" "tfstate" {
   }
 }
 
-resource "aws_dynamodb_table" "tfstate_lock" {
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
   name         = "docker-image-4codebuild-tfstate-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
@@ -31,5 +40,15 @@ resource "aws_dynamodb_table" "tfstate_lock" {
 
   tags = {
     Project = "StreamlinedImageCreation"
+  }
+}
+
+# Explicitly define the S3 backend
+terraform {
+  backend "s3" {
+    bucket         = "docker-image-4codebuild-tfstate"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "docker-image-4codebuild-tfstate-lock"
   }
 } 
