db61487582aaf7a4befb06af2b2fcba52a548b76
Chauncey Yan <chaunceyyann@gmail.com>
2025-05-23 21:28:27 -0400

    bad idea for state and lock

--- terraform/state_backend.tf
+++ terraform/state_backend.tf
@@ -1,12 +1,28 @@
 # Note: The S3 bucket and DynamoDB table for state and lock are already created.
 # Terraform will use these existing resources for state management.
 
+# If the DynamoDB table does not exist, uncomment the following to create it:
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "docker-image-4codebuild-tfstate-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Project = "StreamlinedImageCreation"
+  }
+}
+
 # Explicitly define the S3 backend to use existing resources
 terraform {
   backend "s3" {
     bucket         = "docker-image-4codebuild-tfstate"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "docker-image-4codebuild-tfstate-lock"
+    # dynamodb_table parameter removed to disable locking temporarily for testing
   }
 } 
