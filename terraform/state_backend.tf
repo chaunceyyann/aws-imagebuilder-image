cc52c8f7cb7f2a61a79240608bfd2441b615070c
Chauncey Yan <chaunceyyann@gmail.com>
2025-07-08 23:34:18 -0400

    add encryption to s3 bucket

--- terraform/state_backend.tf
+++ terraform/state_backend.tf
@@ -11,6 +11,27 @@ resource "aws_s3_bucket_versioning" "tfstate" {
   }
 }
 
+# Server-side encryption for the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 # Explicitly define the S3 backend to use existing resources
 terraform {
   backend "s3" {
